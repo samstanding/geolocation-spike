8ef7ca638eebc5e711a7b28485c5fe5079b7e601
Sam Standing <samzstanding@gmail.com>
2018-04-10 17:47:56 -0500

    markers appearing, loading bug

--- server/public/scripts/controllers/addLocation-controller.js
+++ server/public/scripts/controllers/addLocation-controller.js
@@ -1,71 +1,17 @@
-app.controller('AppController', ['$http', function ($http) {
+app.controller('AppController', [ 'LocationService', function ( LocationService) {
     console.log('what up');
     let self = this;
 
     self.newLocation = {};
-    self.locations = {
-        list: []
-    };
+    self.locations = LocationService.locations;
 
-    
-
-    self.getLocations = () => {
-        $http({
-            method: 'GET',
-            url:'/location',
-        }).then((response) => {
-            self.locations.list = response.data.rows;
-        }).catch((error) => {
-            console.log('error on get', error);
-        })
-    }
+    self.getLocations = LocationService.getLocations;
+   
     self.getLocations();
 
-    self.findLocation = function (location) {
-        console.log('in find location');
-        success = function (pos) {
-            console.log('in success');
-            var crd = pos.coords;
+    self.findLocation = LocationService.findLocation;
 
-            console.log('your current position is: ');
-            console.log(`Latitude: ${crd.latitude}`);
-            console.log(`Longitude: ${crd.longitude}`);
-            console.log(`more or less ${crd.accuracy} meters`);
-        $http({
-            method: 'POST',
-            url: '/location',
-            data: {
-                label: location.label,
-                lat: crd.latitude,
-                long: crd.longitude,
-                accuracy: crd.accuracy
-            }
-        }).then((response) => {
-            console.log('sent location to db');
-            self.getLocations();
-        }).catch((error) => {
-            console.log('error on post: ', error);
-        })
-    }
-        error = function (err) {
-            console.log('error on finding location: ', err);
-        }
-        let geoOptions = {
-            enableHighAccuracy: true
-        }
-
-        navigator.geolocation.getCurrentPosition(success, error, geoOptions)
-    }
-
-    self.deleteLocation = (id) => {
-        $http.delete(`/location/${id}`)
-        .then((response) => {
-            self.getLocations();
-        }).catch((error) => {
-            console.log('error on delete:', error);
-            
-        })
-    }
+    self.deleteLocation = LocationService.deleteLocation;
 
 }])
 
--- server/public/scripts/controllers/map-controller.js
+++ server/public/scripts/controllers/map-controller.js
@@ -1,31 +1,47 @@
-app.controller('MapController', [ '$http', '$scope', function ($http) {
-    console.log('in the map');
+app.controller('MapController', ['LocationService', '$scope', function ( LocationService, $scope) {
     let self = this;
     self.map;
     self.marker;
 
+   
+    self.getLocations = LocationService.getLocations;
+    self.getLocations();
+    self.locations = LocationService.locations;
+
+    console.log(self.locations);
+    
+
     self.initMap = function () {
         console.log('in initMap');
 
-        let myLatLng={lat: 44.978114, lng: -93.2633618};
-        
+        let myLatLng = {
+            lat: 44.978114,
+            lng: -93.2633618
+        };
+
         self.map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 44.978, lng: -93.263},
+            center: {
+                lat: 44.978,
+                lng: -93.263
+            },
             zoom: 12,
             streetViewControl: false,
         })
+        for (let i =0; i <self.locations.list.length; i ++) {
+            console.log(self.locations.list[i]);
+            
+            self.marker = new google.maps.Marker({
+                position: new google.maps.LatLng(self.locations.list[i].latitude, self.locations.list[i].longitude),
+                map: self.map,
+                title: self.locations.list[i].location_name,
+            })
+        }
+            
+        
+     
+        
+        }
 
-        self.marker = new google.maps.Marker({
-            position: myLatLng,
-            map: self.map,
-            title: 'me!'
-        })
-        console.log(self.marker);
-        console.log(myLatLng);
-        
-        
-    }
+    self.initMap();
 
-  self.initMap();
-        
-    }])
+}])
