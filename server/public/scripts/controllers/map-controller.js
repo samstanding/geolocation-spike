7abe948fae78c84d51eea162506c33ba45a3c5d1
Sam Standing <samzstanding@gmail.com>
2018-04-10 16:20:39 -0500

    added marker

--- server/public/scripts/controllers/map-controller.js
+++ server/public/scripts/controllers/map-controller.js
@@ -2,16 +2,27 @@ app.controller('MapController', [ '$http', '$scope', function ($http) {
     console.log('in the map');
     let self = this;
     self.map;
+    self.marker;
 
     self.initMap = function () {
         console.log('in initMap');
+
+        let myLatLng={lat: 44.978114, lng: -93.2633618};
         
         self.map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 8, lng: -1},
-            zoom: 4,
+            center: {lat: 44.978, lng: -93.263},
+            zoom: 12,
             streetViewControl: false,
         })
 
+        self.marker = new google.maps.Marker({
+            position: myLatLng,
+            map: self.map,
+            title: 'me!'
+        })
+        console.log(self.marker);
+        console.log(myLatLng);
+        
         
     }
 
