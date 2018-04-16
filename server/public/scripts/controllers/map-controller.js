aedfc203a5e36dd5236a1a3e593de611b0a01649
Sam Standing <samzstanding@gmail.com>
2018-04-16 12:39:22 -0500

    think i reset to when icon was working

--- server/public/scripts/controllers/map-controller.js
+++ server/public/scripts/controllers/map-controller.js
@@ -1,64 +1,79 @@
-app.controller('MapController', ['LocationService', '$scope', function ( LocationService, $scope) {
+app.controller('MapController', ['LocationService', '$scope', function (LocationService, $scope) {
     let self = this;
-   
+
     self.getLocations = LocationService.getLocations;
     self.getLocations();
     self.locations = LocationService.locations;
 
-    self.initMap = function () {
-        console.log('in initMap');
+    let personMarker = new google.maps.Marker();
 
-        let myLatLng = {
-            lat: 44.978114,
-            lng: -93.2633618
-        };
+    self.findLocation = () => {
+        console.log('in find location map');
+        success = (pos) => {
+            let crd = pos.coords;
+            console.log('your current position is: ');
+            console.log(`Latitude: ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+            console.log(`more or less ${crd.accuracy} meters`);
 
-        self.map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 44.978,
-                lng: -93.263
-            },
-            zoom: 18,
-            streetViewControl: false,
-        })
-
-        self.infowindow = new google.maps.InfoWindow();
-
-        for (let i =0; i <self.locations.list.length; i ++) {
-            
-            
-            let marker = new google.maps.Marker({
-                position: new google.maps.LatLng(self.locations.list[i].latitude, self.locations.list[i].longitude),
+            personMarker = {
+                position: new google.maps.LatLng(crd.latitude, crd.longitude),
                 map: self.map,
-                title: self.locations.list[i].location_name,
-            })
+                icon: '../../img/maps_marker.png',
+            }
+        $scope.$apply();
+    }
+    error = (err) => {
+        console.log('error in finding location: ', err);
+    }
+    // options = {
+    //     enableHighAccuracy: true
+    // }
+    navigator.geolocation.watchPosition(success, error);
+}
+self.findLocation();
 
-            google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                return function () {
-                    self.infowindow.setContent(self.locations.list[i].location_name);
-                    self.infowindow.open(self.map, marker );
-                }
-            }) (marker, i));
-            
-        }
+self.initMap = function () {
+    console.log('in initMap');
 
-        navigator.geolocation.watchPosition(function (position) {
-            console.log('in geolocator');
-            
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+    let myLatLng = {
+        lat: 44.978114,
+        lng: -93.2633618
+    };
 
-        let personMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(pos.lat, pos.lng),
+    self.map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 44.978,
+            lng: -93.263
+        },
+        zoom: 18,
+        streetViewControl: false,
+    })
+
+    self.infowindow = new google.maps.InfoWindow();
+
+    for (let i = 0; i < self.locations.list.length; i++) {
+
+
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(self.locations.list[i].latitude, self.locations.list[i].longitude),
             map: self.map,
-            icon: './maps_marker.png'
-        });
+            title: self.locations.list[i].location_name,
         })
-        
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                self.infowindow.setContent(self.locations.list[i].location_name);
+                self.infowindow.open(self.map, marker);
+            }
+        })(marker, i));
+
+    }
+
+
+
 }
 
-    self.initMap();
+self.initMap();
 
 }])
