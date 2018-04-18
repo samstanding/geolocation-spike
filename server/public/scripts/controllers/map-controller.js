ae61b341f19aa50468369fc03532a0df2c736ee9
Sam Standing <samzstanding@gmail.com>
2018-04-18 15:36:27 -0500

    testing timeout and frequency on the geolocation

--- server/public/scripts/controllers/map-controller.js
+++ server/public/scripts/controllers/map-controller.js
@@ -9,54 +9,6 @@ app.controller('MapController', ['LocationService', '$scope', function (Location
 
     let markerStore = {marker: null};
 
-    navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
-        var lastCheckedPosition,
-            locationEventCount = 0,
-            watchID,
-            timerID;
-    
-        options = options || {};
-    
-        var checkLocation = function (position) {
-            lastCheckedPosition = position;
-            locationEventCount = locationEventCount + 1;
-            // We ignore the first event unless it's the only one received because some devices seem to send a cached
-            // location even when maxaimumAge is set to zero
-            if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
-                clearTimeout(timerID);
-                navigator.geolocation.clearWatch(watchID);
-                foundPosition(position);
-            } else {
-                geoprogress(position);
-            }
-        };
-    
-        var stopTrying = function () {
-            navigator.geolocation.clearWatch(watchID);
-            foundPosition(lastCheckedPosition);
-        };
-    
-        var onError = function (error) {
-            clearTimeout(timerID);
-            navigator.geolocation.clearWatch(watchID);
-            geolocationError(error);
-        };
-    
-        var foundPosition = function (position) {
-            geolocationSuccess(position);
-        };
-    
-        if (!options.maxWait)            options.maxWait = 120000; // Default 10 seconds
-        if (!options.desiredAccuracy)    options.desiredAccuracy = 45; // Default 20 meters
-        if (!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
-    
-        options.maximumAge = 0; // Force current locations only
-        options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
-    
-        watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
-        timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
-    };
-
     self.findLocation = () => {
         console.log('in find location map');
         success = (pos) => {
@@ -92,10 +44,10 @@ app.controller('MapController', ['LocationService', '$scope', function (Location
 
     options = {
         enableHighAccuracy: true,
-        accuracy: 45,
-        maxWait: 1500000
+        frequency: 1,
+        timeout: 6000,
     }
-    navigator.geolocation.getAccurateCurrentPosition(success, error, geoprogress, options);
+    navigator.geolocation.watchPosition(success, error, options);
 }
 self.findLocation();
 
